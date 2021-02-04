984b6b2de52bb012f327713498f5390bbf523723
chuseman <chris@huseman.net>
2021-02-04 08:20:25 -0600

    4 files modified, 6 added, 3 deleted

--- app.module.ts
+++ app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { GridAllModule } from "@syncfusion/ej2-angular-grids";
 
-import { AppComponent } from "../app.component";
-import { DemoGridComponent } from "../demo-grid.component";
+import { AppComponent } from "./app.component";
+import { View1Component } from "./view1.component";
+import { View2Component } from "./view2.component";
 
 @NgModule({
-  declarations: [AppComponent, DemoGridComponent],
+  declarations: [AppComponent, View1Component, View2Component],
   imports: [BrowserModule, CommonModule, GridAllModule],
-  bootstrap: [AppComponent],
-  entryComponents: [DemoGridComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
